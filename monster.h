085cf610a40d2161866b10fe98dba6880cc997f6
MarinoA <36242627+MarinoA@users.noreply.github.com>
2019-03-22 10:50:48 -0400

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,11 @@
+#include "project_settings.h"
+#include "random_int.h"
+#include "stddef.h"
+#include "strings.h"
+#include "game.h"
+#include "timing.h"
+#include "task.h"
+#include "terminal.h"
+#include "random_int.h"
+
+void monsterGame_init(void);
